5b6de9a1a865f523251bdf93c7c90250d50f856a
Shan Qing <s_qing@umail.cusb.edu>
2019-11-20 22:30:40 -0800

    sq -added code to retrieve earthquake data from USGS web service and fixed controller

--- src/main/java/hello/WebController.java
+++ src/main/java/hello/WebController.java
@@ -50,17 +50,17 @@ public class WebController {
         return "page2";
     }
 
+     @GetMapping("/earthquakes/search")
+    public String getEarthquakesSearch(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
+            EqSearch eqSearch) {
+        return "earthquakes/search";
+    }
+
     @GetMapping("/earthquakes/results")
     public String getEarthquakesResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
             EqSearch eqSearch) {
-
-        EarthquakeQueryService e =
-           new EarthquakeQueryService();
-
         model.addAttribute("eqSearch", eqSearch);
         // TODO: Actually do the search here and add results to the model
-        String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag());
-        model.addAttribute("json", json);
         return "earthquakes/results";
     }
 
