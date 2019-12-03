e93dd7d9116030c0717354bba1d485d30ec9a2d1
卿山 <shanqing@Shans-super-MacBook-Pro.local>
2019-12-03 00:54:23 -0800

    sq - Added location search and return location

---
+++ src/main/java/earthquakes/searches/LocSearch.java
@@ -0,0 +1,18 @@
+package earthquakes.searches;
+
+public class LocSearch{
+
+    private String location;
+    
+    LocSearch(){
+    }
+
+    public String getLocation(){
+	    return this.location;
+    }
+    
+    public void setLocation(String l){
+	this.location = l;
+    }
+
+}
--- src/test/java/earthquakes/HomePageTest.java
+++ src/test/java/earthquakes/HomePageTest.java
@@ -66,12 +66,12 @@ public class HomePageTest {
     public void getHomePage_hasCorrectTitle() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
-                .andExpect(xpath("//title").exists())
-                .andExpect(xpath("//title").string("CS56 Spring Boot Practice App"));
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[1]/a").exists())
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[1]/a").string("Earthquake Search"));
     }
 
     @Test
-    public void getHomePage_hasCorrectBrand() throws Exception {
+    public void getHomePage_hasEarthquakesLink() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
                 .andExpect(xpath("/html/body/div/nav/a").exists())
@@ -79,10 +79,18 @@ public class HomePageTest {
     }
 
     @Test
-    public void getHomePage_hasCorrectMenu() throws Exception {
+    public void getHomePage_hasLocationsLink() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
                 .andExpect(status().isOk())
                 .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").exists())
-                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").string("Earthquake Search"));
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[2]/a").string("Locations"));
+    }
+
+    @Test
+    public void getHomePage_hasUsersLink() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[3]/a").exists())
+                .andExpect(xpath("/html/body/div/nav/div/ul[1]/li[3]/a").string("Users"));
     }
 }
