0bec6a278f4fc3ff88a4e86f917e8051e7de9a5f
卿山 <shanqing@Shans-super-MacBook-Pro.local>
2019-12-03 20:00:48 -0800

    sq - Added location but not actual search

--- src/main/java/earthquakes/searches/EqSearch.java
+++ src/main/java/earthquakes/searches/EqSearch.java
@@ -4,6 +4,10 @@ public class EqSearch{
 
     private int distance;
     private int minmag;
+    private long lat;
+    private long lon;
+    private String location;
+    
     
     public EqSearch(){
 	    this.distance = 0;
@@ -18,6 +22,18 @@ public class EqSearch{
 	    return this.minmag;
     }
 
+    public long getLat(){
+        return this.lat;
+    }
+
+    public long getLon(){
+        return this.lon;
+    }
+
+    public String getLocation(){
+        return this.location;
+    }
+
    
     
     public void setDistance(int d){
@@ -26,5 +42,17 @@ public class EqSearch{
     
     public void setMinmag(int m){
 	this.minmag = m;
+    }
+
+    public void setLat(long la){
+        this.lat = la;
+    }
+
+    public void setLon(long lo){
+        this.lon = lo;
+    }
+
+    public void setLocation(String loc){
+        this.location = loc;
     }
 }
